037b59e09783ddf4a710f27182ffc47c7d49b6e1
HY Tomioka <henriqueyudi2112@gmail.com>
2021-02-08 18:38:57 -0300

    Alteracoes: query com order by e uso do TypedQuery

--- src/main/java/br/com/tomioka/jpa/teste/TesteJPQL.java
+++ src/main/java/br/com/tomioka/jpa/teste/TesteJPQL.java
@@ -5,7 +5,7 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-import javax.persistence.Query;
+import javax.persistence.TypedQuery;
 
 import br.com.tomioka.jpa.modelo.Conta;
 import br.com.tomioka.jpa.modelo.Movimentacao;
@@ -21,18 +21,19 @@ public class TesteJPQL {
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("tomioka");
 		EntityManager em = emf.createEntityManager();
 		
-		String jpql = "select m from Movimentacao m where m.conta = :pConta";
+		String jpql = "select m from Movimentacao m where m.conta = :pConta order by m.valor asc";
 		
 		Conta conta = new Conta();
 		conta.setId(1L);
 		
-		Query query = em.createQuery(jpql);
+		TypedQuery<Movimentacao> query = em.createQuery(jpql, Movimentacao.class);
 		query.setParameter("pConta", conta); // Executa a query com um parametro (objeto conta)
 		List<Movimentacao> rl = query.getResultList();
 		
 		for (Movimentacao movimentacao : rl) {
 			System.out.println("Descrição: " + movimentacao.getDescricao());
 			System.out.println("Tipo: " + movimentacao.getTipoMovimentacao());
+			System.out.println("Valor: " + movimentacao.getValor());
 		}
 		
 	}
